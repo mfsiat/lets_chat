19afb0834d2968471e170d57b80ee61ff843de67
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-15 01:48:57 +0600

    added route functionalities and installed react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,10 @@
 import React from 'react'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <Router path="/" exact component={Join} />
+    <Router path="/chat" component={Chat} />
+  </Router>
+)
