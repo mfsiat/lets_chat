7356cd0be55aa11557ea4f8d2ec2d1b26cad8be9
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-15 01:55:26 +0600

    basic client skeleton done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Join from './components/Join'
+import Chat from './components/Chat'
 
 const App = () => (
   <Router>
     <Router path="/" exact component={Join} />
     <Router path="/chat" component={Chat} />
   </Router>
-)
+);
+
+export default App;
