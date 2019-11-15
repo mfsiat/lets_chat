72f07305637b59c974075d321548b24a4d0edb18
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-15 11:13:07 +0600

    router is being used by our server

--- server/index.js
+++ server/index.js
@@ -7,13 +7,20 @@ const http = require("http");
 // declare the port
 const PORT = process.env.PORT || 5000;
 
+// part 2, we need to require the router that we created 
+const router = require('./router');
+
 const app = express();
 
 // init server
 const server = http.createServer(app);
 const io = socketio(server);
 
+// call it as middleware 
+app.use(router);
+
 // run the server
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
-// Done so far  
+// part 1 Done so far  
+
