1d530112d25c311a0d499a44399ab95f86ff055e
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-15 11:58:34 +0600

    how to setname or how to change the state

--- client/src/Components/Join.js
+++ client/src/Components/Join.js
@@ -1,10 +1,43 @@
-import React from "react";
+// 1. when user join a connection event will fire up
+// 2. when user join a disconnection event will fire up
+// 3. we are using react hooks
+// we can use state and lifeCycle method inside hook at the same time
 
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+// function based component
 const Join = () => {
+  // delcaring hooks
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
-    <h1>Join</h1>
+    <div className="joinOUterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <iinput
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <iinput
+            placeholder="Room"
+            className="joinInput mt-20"
+            type="text"
+            onChange={}
+          />
+        </div>
+        <Link>
+          <button className="button mt-20" type="submit">Sign In</button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
 export default Join;
- 
