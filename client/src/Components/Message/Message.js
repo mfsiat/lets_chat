31e1820b12e0296d2bc2505102f4e1bb054e05d8
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-18 00:51:57 +0600

    updated and added message

--- client/src/Components/Message/Message.js
+++ client/src/Components/Message/Message.js
@@ -1,8 +1,10 @@
+// here message is comming with both user and text
+// message is an object
 import React from 'react';
 
 import './Message.css';
 
-const Message = ({ message, name }) => {
+const Message = ({ message: { user, text }, name }) => {
   let isSentByCurrentUser = false;
 
   const trimmedName = name.trim().toLowerCase();
@@ -11,7 +13,24 @@ const Message = ({ message, name }) => {
   }
 
   return (
-    isSentByCurrentUser ? () : ()
+    isSentByCurrentUser 
+      ? (
+        <div className="messageContainer justifyEnd"> 
+          <p className="sentText pr-10">{trimmedName}</p>
+          <div className="messageBox backgroundBlue">
+            <p className="messageText colorWhite">{text}</p>
+          </div>
+        </div>
+      ) 
+      : (
+        <div className="messageContainer justifyStart"> 
+          <div className="messageBox backgroundLight">
+            <p className="messageText colorDark">{text}</p>
+          </div>
+          <p className="sentText pl-10">{user}</p>          
+        </div>
+
+      )
   )
 }
 
