04a606a1e14224195ab0aee4aed5603fb2f543d6
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-17 13:33:44 +0600

    added users and added helper function to manage users

---
+++ server/users.js
@@ -0,0 +1,39 @@
+// manages users ( add, delete, track, sing in, sing out )
+// will create helper functions 
+
+// array of users 
+const users = [];
+
+// takes 3 paremeters
+const addUser = ({ id, name, room }) => {
+  // we need to trim the white space and etc and make them all lower case 
+  // we will be adding them in rooms which will have all lower case name
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  // checks for existing user with same username
+  const existingUser = users.find((user) => user.room === room && user.name === name);
+
+  if(existingUser) {
+    return { error: 'Username is taken' };
+  }
+
+  const user = { id, name, room };
+
+  users.push(user);
+
+  return { user };
+}
+
+const removeUser = () => {
+
+}
+
+const getUser = () => {
+
+}
+
+const getUsersInRoom = () => {
+
+}
+
