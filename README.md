2a3ec4fc50733cd343888b9dbf643331fe262620
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-17 12:21:30 +0600

    testing request to servr

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 - We are using socket.io and node for backend and for front end we are using react.
 
--
+- 
 
 ## Join instance
 
@@ -12,3 +12,13 @@
 - when user join a disconnection event will fire up
 - we are using react hooks
 - we can use state and lifeCycle method inside hook at the same timeon Link we are not transferring data with props or using redux we are going to pass data through url or as string
+
+
+## Chat instance
+
+- we are using hooks, socket and useEffect(life cycle method)
+
+- **useEffect** is a hook it is similar to **componentDidMount** and also **componentDidUpdate**. So using just only useEffect is enough to declare two methods. 
+
+- in our chat first thing will happen 
+  - 1 retrive data while joining 
--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+let socket;
+
+// location comes from react router
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
+  // it will run when the component renders
+  // first retrive the data while joining
+  // we are passing in our location search data
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    console.log(socket);
+    
+  });
   return (
     <h1>Chat</h1>
   );
