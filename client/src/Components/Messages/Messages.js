76ca6d10b5072ca9da971577589be182abf70333
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-17 22:35:14 +0600

    messages done

---
+++ client/src/Components/Messages/Messages.js
@@ -0,0 +1,18 @@
+// this will show the messages
+// here we are fetching messages from chat.js 
+// it is mapped and comming as a component with an index 
+// basically we are lopping through the messages 
+// it is lopping through as key
+// Message willl come with message and the user who is sending like their name
+import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+import './messages.css';
+
+const Messages = ({ messages, name }) => (
+  <ScrollToBottom>
+    {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
+  </ScrollToBottom>
+)
+
+export default Messages;
